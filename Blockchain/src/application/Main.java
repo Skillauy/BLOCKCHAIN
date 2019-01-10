9966e56daf0e6980c1682dd9e6ef603bf77d191c
Skillauy <pofu@mailapps.online>
2019-01-10 13:49:52 +0100

    Aktualisiert
    Aktualisiert 10.01.

--- Blockchain/src/application/Main.java
+++ Blockchain/src/application/Main.java
@@ -6,14 +6,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
 
-
-/* Das ist ein test */
-
-//TEST22
 
 public class Main extends Application {
 	public static void main(String[] args) {
@@ -22,25 +20,37 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception{
+		/*--------------------------------
+		 * 
+		 * 1 Scene
+		 * 
+		 * --------------------------------*/
 		
 		//Komponenten und Buttons erste Scene
 		Label lb1 = new Label();
 		lb1.setText("1. Scene");
-		stage.setTitle("Menu");
+		
 		Button btnl = new Button();
 		btnl.setText("Show");
 		Button btnr = new Button();
 		btnr.setText("Manual");
 	
-
-		//Layout erste Scene
+    	//Layout erste Scene
 		FlowPane fp1 = new FlowPane();
 		fp1.setHgap(50);
 	    fp1.setVgap(50);
-	    fp1.setPadding(new Insets(60,15,15,60));
-		Scene scene = new Scene(fp1, 370, 150);
+	    fp1.setPadding(new Insets(60,15,15,60));	
 		
-		fp1.getChildren().addAll(btnl, lb1, btnr);
+	/*	//ANCHOR
+		AnchorPane ap = new AnchorPane();
+		ap.setTopAnchor(btnl, 40.0);
+		ap.setBottomAnchor(btnr, 40.0);
+		ap.setRightAnchor(btnl, 40.0);
+		Scene scene = new Scene(ap, 370, 150);
+		ap.getChildren().addAll(btnl, lb1, btnr); */
+	    
+	    fp1.getChildren().addAll(btnl,lb1, btnr);
+	    Scene scene = new Scene(fp1, 350,150);
 
 		
 		//CSS Stylesheet
@@ -49,8 +59,15 @@ public class Main extends Application {
 		//CSS Buttons
 		btnl.setStyle("-fx-border-color: #ff0000; -fx-border-width: 5px;");
 		btnr.setStyle("-fx-border-color: #ff0000; -fx-border-width: 5px;");
+	
 		
-/*---------------------------------------------------------------------*/
+		stage.setTitle("Menu");
+		
+		/*--------------------------------
+		 * 
+		 * 2 Scene
+		 * 
+		 * --------------------------------*/	    
 		
 		//Komponenten fuer zweiten Scene
 		final Label lb2 = new Label();
@@ -69,7 +86,7 @@ public class Main extends Application {
 	
 		fp2.getChildren().addAll(back2, lb2, anima);
 		
-		stage.setScene(scene);
+		stage.setScene( scene );
 		stage.show();
 		
 		//CSS Stylesheet
@@ -79,7 +96,12 @@ public class Main extends Application {
 		anima.setStyle("-fx-border-color: #ff0000; -fx-border-width: 5px;");
 		back2.setStyle("-fx-border-color: #ff0000; -fx-border-width: 5px;");
 		
-/*---------------------------------------------------------------------*/
+		
+		/*--------------------------------
+		 * 
+		 * 3 Scene
+		 * 
+		 * --------------------------------*/	
 		
 		//Komponenten fuer dritte Scene
 	    Label lb3 = new Label();
@@ -104,7 +126,12 @@ public class Main extends Application {
 	  	//CSS Buttons
 	    back3.setStyle("-fx-border-color: #ff0000; -fx-border-width: 5px;");
 	    
-/*---------------------------------------------------------------------*/	    
+	    
+	    /*--------------------------------
+	     * 
+	     * Scene 4
+	     * 
+	     * --------------------------------*/	    
 	    
 	    //Elemente fuer vierte Scene (manuel, noch leer)
 	    Label lb4 = new Label();
@@ -129,7 +156,11 @@ public class Main extends Application {
 	    //CSS Buttons
 	  	back4.setStyle("-fx-border-color: #ff0000; -fx-border-width: 5px;");
 	    
-/*---------------------------------------------------------------------*/	    
+		/*--------------------------------
+		 * 
+		 * Event Handler
+		 * 
+		 * --------------------------------*/	    
 	    
 		//HANDLER
 	  	
@@ -143,6 +174,7 @@ public class Main extends Application {
 			stage.setScene(secondScene);
 			stage.setTitle("init Show");
 		});
+	  	
 		//Animation Button
 		anima.setOnAction(e -> {
 			stage.setScene(thirdScene);
